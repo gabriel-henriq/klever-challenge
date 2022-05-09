ddd2938292447ea2913916c3ea83250ed9ff811a
Gabriel Henrique <gabrielhcm01@gmail.com>
2022-05-09 13:07:06 -0300

    Creating models for schema doc in mongodb with bson, json and structured support.

---
+++ models/models.go
@@ -0,0 +1,10 @@
+package models
+
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
+type Book struct {
+	ID     primitive.ObjectID `bson:"_id,omitempty" json:"id"`
+	Title  string             `bson:"title,omitempty" json:"title"`
+	Author string             `bson:"author,omitempty" json:"author"`
+	Likes  int64              `bson:"likes,omitempty" json:"likes"`
+}
