3de4796d8fc0704bd1f03b5165c036917a021bad
Gabriel Henrique <gabrielhcm01@gmail.com>
2022-05-09 22:57:55 -0300

    Removing guide challenge in main file.

--- server/main.go
+++ server/main.go
@@ -1,15 +1,3 @@
-//  API with Golang using gRPC with stream pipes that exposes an upvote service endpoints.
-
-// - The API must guarantee the typing of user inputs.
-
-// - If an input is expected as a string, it can only be received as a string.
-
-// - The structs used with your mongo model should support Marshal/Unmarshal with bson, json and struct
-
-// - The API should contain unit test of methods it uses
-
-// - Deliver the whole solution running in some free cloud service
-
 package main
 
 import (
